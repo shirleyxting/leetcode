a8a4660fe62fcdb65426dfa6f035161b9af415c0
xuetingandyang <gracefulrane@163.com>
2019-12-29 18:54:40 -0800

    update xueting

--- quoraOA/constructName.py
+++ quoraOA/constructName.py
@@ -3,7 +3,18 @@ __author__ = ' Zhen Wang'
 
 class Solution:
 
-    def check(self, a: str, b: str) -> bool:
+    def check(self, a: str, b:str) -> bool:
+        """
+        ?: If two strings are close enough.
+        Given two rules to define two strings are close enough.
+        1. you can swap neighbor char any times. Ex. "abb" -> "bba"
+        2. If two strings have the same character, then you can change the character into another.
+            Ex. If both strings contain "a" and "b", you can change all "a"s in the first string or change all "b"s in the first string. same as the second string
+
+        Ex.
+        Input: S1 = "babzccc", S2 = "abbzczz"
+        Output: True
+        """
         if a is None and b is None:
             return True
 
@@ -22,18 +33,20 @@ class Solution:
         for c in b:
             map_b[c] = map_b[c] + 1 if c in map_b else 1
 
+        # check if 'a' and 'b' contain SAME characters
         for k in map_a:
             if k not in map_b:
                 return False
 
+        # count frequency, if under SAME freq, have SAME characters --> True
         count_a = {}
         count_b = {}
 
-        for k in map_a:
-            count_a[k] = count_a[k] + 1 if k in count_a else 1
+        for _, freq in map_a.items():
+            count_a[freq] = count_a[freq] + 1 if freq in count_a else 1
 
-        for k in map_a:
-            count_b[k] = count_b[k] + 1 if k in count_b else 1
+        for _, freq in map_b.items():
+            count_b[freq] = count_b[freq] + 1 if freq in count_b else 1
 
         for k in count_a:
             if count_a[k] != count_b[k]:
