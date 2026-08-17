9bf5906464cb3cb6ab40c2c9dd09d46ed3397694
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-16 21:47:55 -0700

    Create 1882-TheNumberOfEmployeesWhichReportToEachEmployee

---
+++ 1882-TheNumberOfEmployeesWhichReportToEachEmployee/1882-TheNumberOfEmployeesWhichReportToEachEmployee.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/16/2026, 9:47:55 PM
+# Write your MySQL query statement below
+select e1.employee_id, e1.name, 
+    count(distinct e2.employee_id) as reports_count,
+    round(avg(e2.age)) as average_age
+from Employees e1 join Employees e2
+    on e1.employee_id = e2.reports_to
+group by e1.employee_id
+order by e1.employee_id
