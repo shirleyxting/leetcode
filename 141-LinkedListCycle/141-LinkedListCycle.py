94e4780766786dc444c8ec7bce4fd61344044cf9
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-16 22:01:05 -0700

    File updated at 8/16/2026, 10:01:04 PM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -1,35 +1,21 @@
-# Last updated: 8/16/2026, 9:52:04 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # if not head or not head.next: return False
-
-        # visited = []
-        # pos = 0
-
-        # curr = head
-        # while curr.next:
-        #     if curr not in visited:
-        #         visited.append(curr)
-        #         curr = curr.next
-        #         pos += 1
-        #     else:
-        #         return True
-        
-        # return False
-
-        # two pointers, hare and tortoise
-        # if there is a cycle, slow will catch fast eventually
-        slow, fast = head, head
-        while slow and fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast: return True
-
-        return False
-        
+# Last updated: 8/16/2026, 10:01:04 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.next = None
+6
+7class Solution:
+8    def hasCycle(self, head: Optional[ListNode]) -> bool:
+9        # Floyd cycle
+10        fast, slow = head, head
+11
+12        while fast and fast.next:
+13            fast = fast.next.next
+14            slow = slow.next
+15
+16            if slow == fast:
+17                return True
+18        
+19        return False
+20
