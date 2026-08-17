579f80bc16f15c58138a0013298b45621d6719d9
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-16 21:50:03 -0700

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.java
@@ -0,0 +1,34 @@
+// Last updated: 8/16/2026, 9:50:02 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot != null) return false;
+        if(root == null && subRoot == null) return true;
+        if(root != null && subRoot == null) return true;
+
+        if(isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode a, TreeNode b) {
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
+
+        return (a.val == b.val) && isSameTree(a.left, b.left) && isSameTree(a.right, b.right);
+    }
+}
