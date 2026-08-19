133e98811d2e3f366b6809a0f91f6194ff4970f2
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-18 17:57:11 -0700

    File updated at 8/18/2026, 5:57:11 PM

--- 136-SingleNumber/136-SingleNumber.py
+++ 136-SingleNumber/136-SingleNumber.py
@@ -1,41 +1,10 @@
-# Last updated: 8/16/2026, 9:52:09 PM
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        # seen = set()
-
-        # for num in nums:
-        #     if num not in seen:
-        #         seen.add(num)
-        #     else:
-        #         seen.remove(num)
-        # res = 0
-        # for num in seen:
-        #     res = num
-        # return res
-
-        # # method 2: ordering
-        # # 1 1 2 3 3
-        # # 1 1 2 2 3
-        # nums.sort()
-        # i = 0
-        # res = 0
-        # while i < len(nums) - 1:
-        #     if nums[i] == nums[i + 1]:
-        #         i += 2
-        #     else:
-        #         res = nums[i]
-        #         break
-        # if i == len(nums) - 1: 
-        #     return nums[i]
-        # return res
-
-        # method 3: bitwise XOR
-        # 0^a=a, a^a=0, a^a^a=a
-        # a^b^c^a^c = a^a^c^c^b  = 0^b = b
-        res = 0
-        for num in nums:
-            res ^= num
-        
-        return res
-
-            
+# Last updated: 8/18/2026, 5:57:11 PM
+1class Solution:
+2    def singleNumber(self, nums: List[int]) -> int:
+3        # xor
+4        res = 0
+5
+6        for num in nums:
+7            res ^= num
+8        
+9        return res
