5c50b5854254c48f9a30670f5f79bbd8267ab2b8
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-23 16:53:10 -0700

    File updated at 8/23/2026, 4:53:10 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,4 +1,4 @@
-# Last updated: 8/23/2026, 4:50:22 PM
+# Last updated: 8/23/2026, 4:53:10 PM
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
 3        m, n = len(board), len(board[0])
@@ -20,17 +20,20 @@
 19            # iterate 4 dirs for word[idx+1]
 20            found = False
 21            for dr, dc in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
-22                found |= backtrack(i + dr, j + dc, idx + 1)
-23            
-24            # cancel selection
-25            board[i][j] = temp
-26
-27            return found
-28        
-29        # iterate all possible start points
-30        for i in range(m):
-31            for j in range(n):
-32                if backtrack(i, j, 0):
-33                    return True
-34        return False
-35        
+22                # found |= backtrack(i + dr, j + dc, idx + 1)   # slow, break immedaitely when found it
+23                if backtrack(i + dr, j + dc, idx + 1):
+24                    found = True
+25                    break
+26            
+27            # cancel selection
+28            board[i][j] = temp
+29
+30            return found
+31        
+32        # iterate all possible start points
+33        for i in range(m):
+34            for j in range(n):
+35                if backtrack(i, j, 0):
+36                    return True
+37        return False
+38        
