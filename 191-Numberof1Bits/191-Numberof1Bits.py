d94cf034f8e586a7d7853ed6372f8f107fe0cf6b
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-31 22:12:02 -0700

    File updated at 8/31/2026, 10:12:02 PM

--- 191-Numberof1Bits/191-Numberof1Bits.py
+++ 191-Numberof1Bits/191-Numberof1Bits.py
@@ -1,8 +1,8 @@
-# Last updated: 8/31/2026, 10:10:36 PM
+# Last updated: 8/31/2026, 10:12:02 PM
 1class Solution:
 2    def hammingWeight(self, n: int) -> int:
 3        # n & (n-1): clear the rightmost 1
-4        # until n = 2^x (only has ONE 1, in leftmost), & (n-1) -> 0
+4        # for n = 2^x (only has ONE 1, in leftmost), & (n-1) -> 0
 5        count = 0
 6        while n != 0:
 7            n &= n - 1
