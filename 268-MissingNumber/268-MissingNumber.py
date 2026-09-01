bfe6067b04c0f0895e236e5a574cddd89914ea87
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-08-31 22:47:02 -0700

    File updated at 8/31/2026, 10:47:01 PM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,13 +1,20 @@
-# Last updated: 8/31/2026, 10:44:44 PM
+# Last updated: 8/31/2026, 10:47:01 PM
 1class Solution:
 2    def missingNumber(self, nums: List[int]) -> int:
-3        # XOR: nums ^ range(n) -> even occurence = 0, odd occurence = 1 -> the missing one
+3        # # XOR: nums ^ range(n) -> even occurence = 0, odd occurence = 1 -> the missing one
 4
-5        n = len(nums)
-6        res = n     # put n in res first, as nums index [0, n-1]
+5        # n = len(nums)
+6        # res = n     # put n in res first, as nums index [0, n-1]
 7
-8        # we want to achieve: 0^1^2^...^(n-1)^n ^ nums[0]^nums[1]^...^nums[n-1]
-9        for i, num in enumerate(nums):
-10            res ^= i ^ num
+8        # # we want to achieve: 0^1^2^...^(n-1)^n ^ nums[0]^nums[1]^...^nums[n-1]
+9        # for i, num in enumerate(nums):
+10        #     res ^= i ^ num
 11        
-12        return res
+12        # return res
+13
+14
+15        # sum: sum(0,1,2,...n) - sum(nums) = missing
+16        target_sum = sum(list(range(len(nums) + 1)))
+17        # target_sum = (n * (n-1)) // 2
+18
+19        return target_sum - sum(nums)
