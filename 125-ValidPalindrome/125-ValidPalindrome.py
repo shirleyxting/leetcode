1203c8f6ac782aba09ae37301ec8a604b6de52b7
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-09-16 16:07:19 -0700

    File updated at 9/16/2026, 4:07:19 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,31 +1,18 @@
-# Last updated: 8/16/2026, 9:52:17 PM
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        # isalnum: check if its alphanumeric (letter or digit)
-
-        # l = 0
-        # r = len(s) - 1
-        # while(l < r):
-        #     if not s[l].isalnum(): l += 1
-        #     elif not s[r].isalnum(): r -= 1
-        #     else:
-        #         if s[l].lower() == s[r].lower():
-        #             l += 1
-        #             r -= 1
-        #         else:
-        #             return False
-        
-        # return s[l].lower() == s[r].lower()
-
-        # clean the string first
-        newS = ''.join(c.lower() for c in s if c.isalnum())
-        l, r = 0, len(newS) - 1
-
-        if len(newS) <= 1: return True
-
-        while(l < r):
-            if newS[l] != newS[r]: return False
-            l += 1
-            r -= 1
-        
-        return newS[l] == newS[r]
+# Last updated: 9/16/2026, 4:07:19 PM
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        left, right = 0, len(s) - 1
+4
+5        while left < right:
+6            while left < right and not s[left].isalnum():
+7                left += 1
+8            while left < right and not s[right].isalnum():
+9                right -= 1
+10            
+11            if s[left].lower() != s[right].lower():
+12                return False
+13            
+14            left += 1
+15            right -= 1
+16        
+17        return True
