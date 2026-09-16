a60922f1b36dcef3c1ff6ee6592979566b29f1e0
shirleyxting <31720647+shirleyxting@users.noreply.github.com>
2026-09-16 11:58:01 -0700

    File updated at 9/16/2026, 11:58:00 AM

--- 100-SameTree/100-SameTree.py
+++ 100-SameTree/100-SameTree.py
@@ -1,43 +1,16 @@
-# Last updated: 8/16/2026, 9:52:36 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # # p.val = q.val, true=isSameTree(p.left, q.left), true=isSameTree(p.right, q.right)
-
-        # if not p and not q: return True
-        # if (not p and q) or (p and not q): return False
-        # if p.val != q.val: return False
-
-        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)   
-
-        # Method2 - BFS
-        if not p and not q: return True
-        if (not p and q) or (p and not q): return False
-
-        p_queue, q_queue = deque(), deque()
-        p_queue.append(p)
-        q_queue.append(q)
-
-        while p_queue and q_queue:
-            i = p_queue.popleft()
-            j = q_queue.popleft()
-            
-            if (not i and j) or (i and not j): 
-                return False
-            elif (i and j) and (i.val != j.val): 
-                return False
-            elif (i and j) and (i.val == j.val):
-                p_queue.append(i.left)
-                p_queue.append(i.right)
-                q_queue.append(j.left)
-                q_queue.append(j.right)
-        
-        if p_queue or q_queue: return False
-
-        return True
-
+# Last updated: 9/16/2026, 11:58:00 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isSameTree(self, p: TreeNode | None, q: TreeNode | None) -> bool:
+9        if not p and not q:
+10            return True
+11        
+12        if not p or not q or p.val != q.val:
+13            return False
+14        
+15        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
